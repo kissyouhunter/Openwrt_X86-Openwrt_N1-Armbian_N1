dd1383059f58fcc0fd5de56db60d9bd9fe0da40f
kissyouhunter <63334206+kissyouhunter@users.noreply.github.com>
2021-09-28 01:17:08 +0800

    Update update-N1.sh

--- update_scripts/update-N1.sh
+++ update_scripts/update-N1.sh
@@ -22,13 +22,13 @@ TIME() {
 
 	while :; do
 	TIME g "---------------------------------------------------"
-	TIME g "[1] 更新至 内核 5.14.7 版本"
+	TIME g "[1] 更新至 内核 5.14.8 版本"
 	echo
 	TIME g "[2] 更新至 内核 5.13.19 版本"
 	echo
-	TIME g "[3] 更新至 内核 5.10.68 版本"
+	TIME g "[3] 更新至 内核 5.10.69 版本"
 	echo
-	TIME g "[4] 更新至 内核 5.4.148 版本"
+	TIME g "[4] 更新至 内核 5.4.149 版本"
 	echo
 	TIME g "[0] 不知道选啥就按0看看会有啥惊喜没?"
 	TIME g "---------------------------------------------------"
@@ -36,12 +36,12 @@ TIME() {
 	case $CHOOSE in
 	1)
 	echo
-	TIME y "[1] 更新至 内核 5.14.7 版本"
+	TIME y "[1] 更新至 内核 5.14.8 版本"
         cd /mnt/mmcblk2p4
         rm -rf update-*.sh openwrt_*
-        url=https://github.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/releases/download/5.4.148_5.10.68_5.14.7
-        Firmware=openwrt_s905d_n1_R21.9.18_k5.14.7-kissyouhunter-docker.img.gz
-        img=openwrt_s905d_n1_R21.9.18_k5.14.7-kissyouhunter-docker.img
+        url=https://github.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/releases/download/5.4.149_5.10.69_5.14.8
+        Firmware=openwrt_s905d_n1_R21.9.18_k5.14.8-kissyouhunter-docker.img.gz
+        img=openwrt_s905d_n1_R21.9.18_k5.14.8-kissyouhunter-docker.img
 	TIME g "=====================下载固件中(需科学上网,否则无法更新)======================"
         curl -LO $url/$Firmware
 	wget https://raw.githubusercontent.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/main/update-amlogic-openwrt.sh
@@ -72,12 +72,12 @@ TIME() {
 	;;
 	3)
 	echo
-	TIME y "[3] 更新至 内核 5.10.68 版本"
+	TIME y "[3] 更新至 内核 5.10.69 版本"
         cd /mnt/mmcblk2p4
         rm -rf update-*.sh openwrt_*
-        url=https://github.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/releases/download/5.4.148_5.10.68_5.14.7
-        Firmware=openwrt_s905d_n1_R21.9.18_k5.10.68-kissyouhunter-docker.img.gz
-        img=openwrt_s905d_n1_R21.9.18_k5.10.68-kissyouhunter-docker.img
+        url=https://github.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/releases/download/5.4.149_5.10.69_5.14.8
+        Firmware=openwrt_s905d_n1_R21.9.18_k5.10.69-kissyouhunter-docker.img.gz
+        img=openwrt_s905d_n1_R21.9.18_k5.10.69-kissyouhunter-docker.img
 	TIME g "=====================下载固件中(需科学上网,否则无法更新)======================"
         curl -LO $url/$Firmware
 	wget https://raw.githubusercontent.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/main/update-amlogic-openwrt.sh
@@ -93,9 +93,9 @@ TIME() {
 	TIME y "[4] 更新至 内核 5.4.148 版本"
         cd /mnt/mmcblk2p4
         rm -rf update-*.sh openwrt_*
-        url=https://github.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/releases/download/5.4.148_5.10.68_5.14.7
-        Firmware=openwrt_s905d_n1_R21.9.18_k5.4.148-kissyouhunter-docker.img.gz
-        img=openwrt_s905d_n1_R21.9.18_k5.4.148-kissyouhunter-docker.img
+        url=https://github.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/releases/download/5.4.149_5.10.69_5.14.8
+        Firmware=openwrt_s905d_n1_R21.9.18_k5.4.149-kissyouhunter-docker.img.gz
+        img=openwrt_s905d_n1_R21.9.18_k5.4.149-kissyouhunter-docker.img
 	TIME g "=====================下载固件中(需科学上网,否则无法更新)======================"
         curl -LO $url/$Firmware
 	wget https://raw.githubusercontent.com/kissyouhunter/Openwrt_X86-Openwrt_N1-Armbian_N1/main/update-amlogic-openwrt.sh
